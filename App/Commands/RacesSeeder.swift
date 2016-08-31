d1a22af522f1b22edd28b086ba50198b2e3735b0
Casper Rasmussen <cr@nodes.dk>
2016-08-31 22:49:04 +0200

    rss in progress

--- App/Commands/RacesSeeder.swift
+++ App/Commands/RacesSeeder.swift
@@ -16,44 +16,45 @@ public final class RacesSeeder: Command {
     public func run(arguments: [String]) throws {
         
         console.info("Started the seeder for races");
-        /*
-        let races = [
-            
-            Race(
-                name: "Copenhagen",
-                startAt: "2016-08-21 07:00:00",
-                isActive: true,
-                liveDataProvider: "ultimate",
-                liveDataEventId: "1",
-                liveStreamUrl: nil
-            ),
-            Race(
-                name: "Aarhus",
-                startAt: "2016-09-10 13:00:00",
-                isActive: true,
-                liveDataProvider: "ultimate",
-                liveDataEventId: "1",
-                liveStreamUrl: nil
-            ),
-            Race(
-                name: "Kronborg",
-                startAt: "2016-06-19 08:00:00",
-                isActive: true,
-                liveDataProvider: "ultimate",
-                liveDataEventId: "1",
-                liveStreamUrl: nil
- 
-        ]
         
+        let races = [
+            try Race(node: [
+                "id": 26,
+                "name": "Copenhagen",
+                "btn_image_path": nil,
+                
+                "start_at": "2016-08-21 07:00:00",
+                "live_data_provider": "ultimate",
+                "live_data_event_id": 3326,
+                "live_stream_url": nil,
+                "youtube_channel": "IRONMANCopenhagen",
+          
+                "is_live_stream_enabled": false,
+                "is_live_stream_active": false,
+                "is_geo_tracking_enabled": true,
+                
+                "sponsor_image_path": nil,
+                "event_info_image_path": nil,
+                "event_text": "KMD IRONMAN Copenhagen - “the most spectacular race course in the world”",
+                "tourist_app_link_ios": nil,
+                "tourist_app_link_android": nil,
+                "fb_page": "IronmanCopenhagen",
+                "hashtag": "#kmdironmancopenhagen",
+                "is_active": true,
+                "created_at": "2013-10-19 22:30:58",
+                "updated_at": "2016-07-20 15:24:38"
+            ])
+        ]
+   
         races.forEach({
             var race = $0
             do {
                 try race.save()
             } catch {
                 console.error("Failed to store \(race.name)")
+                print(error)
             }
         })
- )*/
         
         console.info("Finished the seeder for races");
     }
--- App/Models/Races/Race.swift
+++ App/Models/Races/Race.swift
@@ -6,37 +6,85 @@ final class Race: Model {
     var name: String
     var btnImagePath: String?
     
-    
     var startAt: String
-    var isActive: Bool
     var liveDataProvider: String
     var liveDataEventId: String?
     var liveStreamUrl: String?
+    var youtubeChannel: String?
     
+    var isLiveStreamEnabled: Bool
+    var isLiveStreamActive: Bool
+    var isGeoTrackingEnabled: Bool
     
+    var sponsorImagePath: String?
+    var eventInfoImagePath: String?
+    var eventText: String?
+    
+    var touriestAppLinkIos: String?
+    var touriestAppLinkAndroid: String?
+    var fbPage: String?
+    var hashtag: String?
+    
+    var isActive: Bool
+    var createdAt: String
+    var updatedAt: String
+ 
     init(node: Node, in context: Context) throws {
         id = try node.extract("id")
         name = try node.extract("name")
-        
-        
+        btnImagePath = try? node.extract("btb_image_path");
+
         startAt = try node.extract("start_at")
-        isActive = try node.extract("is_active")
         liveDataProvider = try node.extract("live_data_provider")
         liveDataEventId = try? node.extract("live_data_event_id")
         liveStreamUrl = try? node.extract("live_stream_url")
+        youtubeChannel = try? node.extract("youtube_channel")
         
+        isLiveStreamEnabled = try node.extract("is_live_stream_enabled")
+        isLiveStreamActive = try node.extract("is_live_stream_active")
+        isGeoTrackingEnabled = try node.extract("is_geo_tracking_enabled")
+        
+        sponsorImagePath = try? node.extract("sponsor_image_path")
+        eventInfoImagePath = try? node.extract("event_info_image_path")
+        eventText = try? node.extract("event_text")
+        
+        touriestAppLinkIos = try? node.extract("tourist_app_link_ios")
+        touriestAppLinkAndroid = try? node.extract("tourist_app_link_android")
+        fbPage = try? node.extract("fb_page")
+        hashtag = try? node.extract("hashtag")
+        
+        isActive = try node.extract("is_active")
+        createdAt = try node.extract("created_at")
+        updatedAt = try node.extract("updated_at")
     }
-    
     
     func makeNode() throws -> Node {
         return try Node(node: [
+            
             "id": id,
             "name": name,
+            "btn_image_path": btnImagePath,
+            
             "start_at": startAt,
+            "live_data_provider": liveDataProvider,
+            "live_data_event_id": liveDataEventId,
+            "live_stream_url": liveStreamUrl,
+            "youtube_channel": youtubeChannel,
+            
+            "is_live_stream_enabled": isLiveStreamEnabled,
+            "is_live_stream_active": isLiveStreamActive,
+            "is_geo_tracking_enabled": isGeoTrackingEnabled,
+            
+            "sponsor_image_path": sponsorImagePath,
+            "event_info_image_path": eventInfoImagePath,
+            "event_text": eventText,
+            "tourist_app_link_ios": touriestAppLinkIos,
+            "tourist_app_link_android": touriestAppLinkAndroid,
+            "fb_page": fbPage,
+            "hashtag": hashtag,
             "is_active": isActive,
-            "live_data_provider" : liveDataProvider,
-            "live_data_event_id" : liveDataEventId,
-            "live_stream_url" : liveStreamUrl
+            "created_at": createdAt,
+            "updated_at": updatedAt
         ])
     }
     
@@ -48,29 +96,28 @@ final class Race: Model {
             "btn_image_url": nil,
             "sponsor_image_url": nil,
             "event_info_image_url" : nil,
-            "event_text": nil,
+            "event_text": eventText,
             "start_datetime": startAt,
             "live_data_provider": liveDataProvider,
             "live_data_provider_event_id": liveDataEventId,
-            "is_geo_tracking_enabled": false,
-            "is_live_stream_enabled": false,
-            "is_live_stream_active": false,
-            "hash_tag":nil,
+            "is_geo_tracking_enabled": isGeoTrackingEnabled,
+            "is_live_stream_enabled": isLiveStreamEnabled,
+            "is_live_stream_active": isLiveStreamActive,
+            "hash_tag": hashtag,
             "live_stream_url": liveStreamUrl,
-            "youtube_channel": nil,
-            "tourist_app_link_ios": nil,
-            "tourist_app_link_android": nil,
+            "youtube_channel": youtubeChannel,
+            "tourist_app_link_ios": touriestAppLinkIos,
+            "tourist_app_link_android": touriestAppLinkAndroid,
             
         ])
         return try JSON(node: data)
     }
     
-    
     static func prepare(_ database: Database) throws {
         try database.create("races") { races in
             races.id()
             races.string("name")
-            races.string("btn_image_id", optional: true)
+            races.string("btn_image_path", optional: true)
             
             races.string("start_at")
             races.string("live_data_provider", optional: true)
@@ -78,7 +125,7 @@ final class Race: Model {
             races.string("live_stream_url", optional: true)
             races.string("youtube_channel", optional: true)
             
-            races.bool("is_live_stream_enbled", optional: true)
+            races.bool("is_live_stream_enabled", optional: true)
             races.bool("is_live_stream_active", optional: true)
             races.bool("is_geo_tracking_enabled", optional: true)
             
