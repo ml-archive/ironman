c264f5205a093f88923af05a3548a19d8ecea452
Morten Rugaard <morten@rugaard.me>
2016-09-12 12:44:39 +0200

    Leaf bug

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     ],
     dependencies: [
         .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 0),
-        .Package(url: "https://github.com/vapor/leaf.git", majorVersion: 0),
+        .Package(url: "https://github.com/vapor/leaf.git", majorVersion: 0, minor: 2),
         .Package(url: "https://github.com/vapor/mysql-provider.git", majorVersion: 0),
         .Package(url: "https://github.com/nodes-vapor/SWXMLHash.git", majorVersion: 3)
     ],
